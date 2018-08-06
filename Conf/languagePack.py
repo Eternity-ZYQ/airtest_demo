54f6eb3b72dc49517df6c083af0cd671e17c6cae
陈子昂 <728661182@qq.com>
2018-08-06 22:49:23 +0800

    Upload setting.py __init__.py languagePack.py

---
+++ Conf/languagePack.py
@@ -0,0 +1,10 @@
+
+class Commond_language(object):
+
+     [init,
+     find_process,
+     install_app,
+     test_msg]=[i for i in range(4)]
+
+     def get_init(self):
+          return Commond_language.init
