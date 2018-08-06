619f4248785895b35b90d8df5a62801fca6d3177
陈子昂 <728661182@qq.com>
2018-08-06 17:02:40 +0800

    更新 platform_base.py

--- base/platform_base.py
+++ base/platform_base.py
@@ -1,4 +1,5 @@
-import platform
+import platform,sys,os,re
+import subprocess
 
 def system_plat():
     "判断是wins还是ios,用于命令行差异"
@@ -7,3 +8,78 @@ def system_plat():
         pass
     else:
         pass
+
+def win_linux_platform():
+    "判断平台的处理"
+    if sys.platform.startswith('linux'):
+        pass
+    elif sys.platform.startswith('win'):
+        pass
+    return sys.platform
+
+def check_task(name):
+    """
+    搜索任务列表
+    :param name: 需要搜索的进程名称
+    :return:
+    """
+    if win_linux_platform() =="Windows":
+        return subprocess.check_output('tasklist | findstr %s'%name, shell=True)
+    else:
+        command = 'ps aux |grep %s' % name
+        return subprocess.check_output(command, shell=True)
+
+
+def color_print(s, color='green'):
+    """
+    颜色输入
+    :param s:
+    :param color:
+    :return:
+    """
+    ct = {
+        'green': '\033[92m',
+        'red': '\033[91m',
+        'none': '\033[0m'
+    }
+    return ''.join([ct[color], s, ct['none']])
+
+
+def show_packages(path):
+    """
+    拿到path目录下所有包含__init__.py的文件夹
+    use:path = r'E:\sendHttp_client\thirdp'
+    :return:
+    """
+    packages = []
+
+    for root, _, files in os.walk(path):
+        if '__init__.py' in files:
+            if win_linux_platform() =="Linux":
+                packages.append(
+                    re.sub('^[^A-z0-9_]', '', root.replace('/', '\\'))
+                )
+            elif win_linux_platform() =="Windows":
+                packages.append(
+                    re.sub('^[^A-z0-9_]', '', root.replace('\\', '\\'))
+                )
+    return packages
+
+
+import threading
+class _Singleton(object):
+    """
+    （多线程）单例类的 装饰器
+    """
+    _LOCK = threading.Lock()
+
+    def __init__(self, cls):
+        self.__instance = None
+        self.__cls = cls
+
+    def __call__(self, *args, **kwargs): #让实例化的对象直接返回结果
+        self._LOCK.acquire()
+        if self.__instance is None:
+            self.__instance = self.__cls(*args, **kwargs)
+        self._LOCK.release()
+        return self.__instance
